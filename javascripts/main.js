d3932c5457016ebbafd1d7a49c259ffea75de1a8
Igor Fedorov <ifedorov@smartru.com>
2014-12-16 15:55:44 +0300

    add soma script for hash change event

---
+++ javascripts/main.js
@@ -0,0 +1,17 @@
+!function(){
+    function hashVal(val){
+        var hashVal = document.querySelector('section span');
+        if(!hashVal){
+            document.querySelector('section').appendChild(hashVal = document.createElement('span'));            
+        }
+        if(val != null)
+            hashVal.innerText = val;
+        
+        return hashVal;
+    }
+    window.addEventListener("hashchange",function(hashevent){
+        hashVal(location.hash.substring(1));
+    });
+    
+    hashVal(location.hash.substring(1));
+}
